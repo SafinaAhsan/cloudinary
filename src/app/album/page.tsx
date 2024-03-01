e076cf78d87256d8a69b2878a459a7ff12904651
SafinaAhsan <safinabagasra@gmail.com>
2024-03-01 16:51:56 +0500

    force-refresh updated

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -2,30 +2,30 @@ import ForceRefresh from "../components/force-refresh"
 import cloudinary from "cloudinary"
 import { AlbumCard } from "../components/shared/album-card"
 
-export type Folders={
-    name:string, path:string
+export type Folders = {
+  name: string, path: string
 }
-const Album = async ()=> {
-  const {folders}= (await cloudinary.v2.api.root_folders() ) as {
-    folders:Folders[]
+const Album = async () => {
+  const { folders } = (await cloudinary.v2.api.root_folders()) as {
+    folders: Folders[]
   }
-     return (
+  return (
     <section>
-    <ForceRefresh/>
-    <div className="px-6 sm:px-0 flex flex-col gap-8">
+      <ForceRefresh />
+      <div className="px-6 sm:px-0 flex flex-col gap-8">
         <div className="flex justify-between">
-        <h1 className="text-4xl font-bold">ALBUMS</h1>
+          <h1 className="text-4xl font-bold">ALBUMS</h1>
         </div>
         <div className="sm:grid grid-cols-3 sm:gap-4">
-        {folders.map((folder)=>
-        <div 
-        key={folder.path}
-        className="mb-4 sm:mb-0"><AlbumCard 
-      
-        folders={folder}
-        /></div>)}
-      </div>
+          {folders.map((folder) =>
+            <div
+              key={folder.path}
+              className="mb-4 sm:mb-0"><AlbumCard
+
+                folders={folder}
+              /></div>)}
         </div>
+      </div>
     </section>
   )
 }
