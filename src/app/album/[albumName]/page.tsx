3f2cf7a713e2c97d285ce8c3220add17041fcb6a
SafinaAhsan <safinabagasra@gmail.com>
2024-03-06 14:01:35 +0500

    force refresh fixed

--- src/app/album/[albumName]/page.tsx
+++ src/app/album/[albumName]/page.tsx
@@ -1,39 +1,38 @@
 import CloudinaryImage from "@/app/components/shared/cloudinary-image"
 import ImageGrid from "@/app/components/shared/image-grid"
 import cloudinary from "cloudinary"
-export type SearchResult={
-    public_id:string
-    tags:string[]
+export type SearchResult = {
+  public_id: string
+  tags: string[]
 }
-const AlbumPage = async ({params:{albumName}}:{
-  params:{albumName:string}
-})=> {
-    const results= (await cloudinary.v2.search
+const AlbumPage = async ({ params: { albumName } }: {
+  params: { albumName: string }
+}) => {
+  const results = (await cloudinary.v2.search
     .expression(`resource_type:image AND folder=${albumName}`)
-    .sort_by("created_at","desc")
+    .sort_by("created_at", "desc")
     .max_results(30)
     .with_field("tags")
-    .execute()) as{resources: SearchResult[]}
-  
-    
+    .execute()) as { resources: SearchResult[] }
+
+
   return (
     <section>
-    {/* <ForceRefresh/> */}
-    <div className="px-6 sm:px-0 flex flex-col gap-8">
+      <div className="px-6 sm:px-0 flex flex-col gap-8">
         <div className="flex justify-between">
-        <h1 className="text-4xl font-bold">Album {albumName}</h1>
-    
+          <h1 className="text-4xl font-bold">Album {albumName}</h1>
+
         </div>
-<ImageGrid images={results.resources}
-getImage={(imageData:SearchResult)=>{
-  return(<CloudinaryImage
-    key={imageData.public_id}  
-    imageData={imageData}
-    width="300"
-    height="400"
-    alt="an image of something"/>)
-}}/>
-  </div>
+        <ImageGrid images={results.resources}
+          getImage={(imageData: SearchResult) => {
+            return (<CloudinaryImage
+              key={imageData.public_id}
+              imageData={imageData}
+              width="300"
+              height="400"
+              alt="an image of something" />)
+          }} />
+      </div>
     </section>
   )
 }
--- src/app/components/shared/upload-button.tsx
+++ src/app/components/shared/upload-button.tsx
@@ -8,34 +8,34 @@ import { setTimeout } from "timers/promises";
 
 
 export const UploadButton = () => {
-  const router=useRouter()
-  const handleUpload=()=>{
+  const router = useRouter()
+  const handleUpload = () => {
     router.refresh()
   }
-   return (
+  return (
     <div>
-        <Button asChild>
-           
-          <CldUploadButton 
-      onUpload={ handleUpload  }
-      uploadPreset="jvqejkno" >
-        <div className="flex gap-2 items-center mb-2 sm:mb-0">
-       <svg xmlns="http://www.w3.org/2000/svg"
-         width="24" 
-         height="24"
-          viewBox="0 0 24 24" 
-          fill="none"
-           stroke="currentColor"
-            stroke-width="2"
-             stroke-linecap="round" 
-             stroke-linejoin="round" 
-             className="lucide lucide-upload">
-                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-                <polyline points="17 8 12 3 7 8"/>
-        <line x1="12" x2="12" y1="3" y2="15"/>
-        </svg>
-        UPLOAD
-        </div>
+      <Button asChild>
+
+        <CldUploadButton
+          onUpload={handleUpload}
+          uploadPreset="jvqejkno" >
+          <div className="flex gap-2 items-center mb-2 sm:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-upload">
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+              <polyline points="17 8 12 3 7 8" />
+              <line x1="12" x2="12" y1="3" y2="15" />
+            </svg>
+            UPLOAD
+          </div>
         </CldUploadButton>
       </Button>
     </div>
